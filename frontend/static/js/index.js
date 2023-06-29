abb11f6dd4e3da226f02ea88bbfac1dee39dabb4
Anthony <anthony.glatt@gmail.com>
2023-06-29 15:49:28 -0700

    Some view(s) setup

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,8 +1,15 @@
+import Dashboard from "./views/Dashboard.js";
+
+const navigateTo = url => {
+  history.pushState(null, null, url)
+  router();
+}
+
 const router = async () => {
   const routes = [
-    { path: '/', view: () => console.log("viewing dashboard")},
-    { path: '/posts', view: () => console.log("viewing posts")},
-    { path: '/settings', view: () => console.log("viewing settings")},
+    { path: '/', view: Dashboard },
+    // { path: '/posts', view: () => console.log("viewing posts")},
+    // { path: '/settings', view: () => console.log("viewing settings")},
   ];
 
   const potentialMatches = routes.map(route => {
@@ -14,9 +21,29 @@ const router = async () => {
 
   let match = potentialMatches.find(potentialMatch => potentialMatch.isMatch);
 
-  console.log(match);
+  if (!match) {
+    match = {
+      route: routes[0],
+      isMatch: true
+    }
+  }
+
+  const view = new match.route.view();
+
+  document.querySelector('#app').innerHTML = await view.getHtml();
+
+  console.log(match.route.view());
 };
 
+window.addEventListener("popstate", router);
+
 document.addEventListener("DOMContentLoaded", () => {
+  document.body.addEventListener("click", e => {
+    if (e.target.matches("[data-link]")) {
+      e.preventDefault();
+      navigateTo(e.target.href);
+    }
+  })
+
   router();
 });
